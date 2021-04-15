0b1227c1702194c694c71e38a58bffa29af52089
mitchelldoruff <mitchell.doruff@gmail.com>
2021-04-15 11:16:26 -0500

    Base for Storyboard

---
+++ Schredule/ModelController.swift
@@ -0,0 +1,30 @@
+//
+//  ModelController.swift
+//  Schredule
+//
+//  Created by Mitchell Doruff on 4/15/21.
+//
+
+import Foundation
+
+class ModelController {
+    //https://developer.apple.com/documentation/foundation/date
+    var exerciseList: MuscleList!
+    var pastWorkouts: PreviousWorkouts!
+    var date: NSDate
+    var stressLevel: StressController!
+    
+    func muscleGroupStress() {
+        
+    }
+    
+    func getDate() -> NSDate {
+        return date
+    }
+    
+    func enterWorkout(Sets: Int, Reps : Int, Weight: Double) {
+        
+    }
+    
+    init () {}
+}
