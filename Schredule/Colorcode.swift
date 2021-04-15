24cb904b38170eb8bafefd26f555734e4b26a926
reinkea426 <82483666+reinkea426@users.noreply.github.com>
2021-04-15 14:12:05 -0500

    Create Colorcode.swift
    Moved to Schredule folder

---
+++ Schredule/Colorcode.swift
@@ -0,0 +1,16 @@
+
+enum Color{
+  case red = "FF0000"
+  case green = "00FF00"
+  case yellow = "00FFFF"
+}
+var yellowCutoff = 5;
+var redCutoff = 2;
+getColor(recovery: Int)-> Color{
+  if recovery > yellowCutoff {
+    return .green;
+  }else if recovery > redCutoff{
+    return .yellow;
+  }
+  return .red;
+}
